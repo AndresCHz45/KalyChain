2163814cf96b15f07e7390454f014efc1768392d
AndresCHz45 <60183029+AndresCHz45@users.noreply.github.com>
2023-09-25 19:24:04 -0500

    Apt

--- src/constants/abis/kalyswapPair.ts
+++
@@ -1,6 +0,0 @@
-import { Interface } from '@ethersproject/abi'
-import { abi as IKalyswapPairABI } from '../../artifacts/kalyswap-core/IKalyswapPair.json'
-
-const KALYSWAP_PAIR_INTERFACE = new Interface(IKalyswapPairABI)
-
-export { KALYSWAP_PAIR_INTERFACE }
--- src/constants/abis/miniChef.ts
+++
@@ -1,6 +0,0 @@
-import { Interface } from '@ethersproject/abi'
-import { abi as MINICHEF_ABI } from './MiniChefV2.json'
-
-const MINICHEF_INTERFACE = new Interface(MINICHEF_ABI)
-
-export { MINICHEF_INTERFACE }
